1a003c71e25ea3b3f8b5eae73b85dc39f2f626b0
Philip Thomsen <pthomsen.ml@gmail.com>
2024-01-30 09:23:20 +0100

    start multihead attention

--- nanoGPT/v2.py
+++ nanoGPT/v2.py
@@ -83,6 +83,16 @@ class Head(nn.Module):
 
     return out
 
+class MultiHeadAttention(nn.Module):
+  """ multiple heads of self-attention in parallel """
+
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    
+  def forward(self, x):
+    return torch.cat([h(x) for h in self.heads], dim=1)
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
