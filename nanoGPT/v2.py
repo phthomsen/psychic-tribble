82377b6d9bfe37fae2ebb846798fa111303de849
Philip Thomsen <pthomsen.ml@gmail.com>
2024-01-30 16:17:49 +0100

    massive scale up, currently not working on macbook

--- nanoGPT/v2.py
+++ nanoGPT/v2.py
@@ -4,14 +4,17 @@ from torch.nn import functional as F
 from tqdm import tqdm
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 120 # 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 64
+n_head = 6
+n_layer = 6
+dropout = 0.2 # 20% is dropped out
 # ------------
 
 torch.manual_seed(1337)
@@ -68,6 +71,7 @@ class Head(nn.Module):
     self.query = nn.Linear(n_embd, head_size, bias=False)
     self.value = nn.Linear(n_embd, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
     B,T,C = x.shape
@@ -77,6 +81,7 @@ class Head(nn.Module):
     wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, T, C) -> (B, T, T)
     wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
     wei = F.softmax(wei, dim=1) # (B, T, T)
+    wei = self.dropout(wei)
     # perform self weighted aggregation of the values
     v = self.value(x)
     out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
@@ -91,10 +96,11 @@ class MultiHeadAttention(nn.Module):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
     self.proj = nn.Linear(n_embd, n_embd)
+    self.dropout = nn.Dropout(dropout)
      
   def forward(self, x):
     out = torch.cat([h(x) for h in self.heads], dim=-1) # concatenate over the channel dimension
-    out = self.proj(out) # projection back into the residual pathway
+    out = self.dropout(self.proj(out)) # projection back into the residual pathway
     return out
 
 class FeedForward(nn.Module):
@@ -105,7 +111,8 @@ class FeedForward(nn.Module):
     self.net = nn.Sequential(
       nn.Linear(n_embd, 4 * n_embd), # grow layer
       nn.ReLU(),
-      nn.Linear(4 * n_embd, n_embd) # shrink layer
+      nn.Linear(4 * n_embd, n_embd), # shrink layer
+      nn.Dropout(dropout)
     )
 
   def forward(self, x):
@@ -137,12 +144,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-          Block(n_embd, n_head=4),
-          Block(n_embd, n_head=4),
-          Block(n_embd, n_head=4),
-          nn.LayerNorm(n_embd)
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -154,6 +157,7 @@ class BigramLanguageModel(nn.Module):
         # x = self.sa_heads(x) # apply one head of self-attention (B, T, C)
         # x = self.ffwd(x) # (B,T,C)
         x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
         if targets is None:
             loss = None
